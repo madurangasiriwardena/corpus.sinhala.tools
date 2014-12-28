46a4edc4e6721dafdde4ed2fe0b836de1df25653
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-12-29 00:12:25 +0530

    packaging corrected

--- src/main/java/corpus/sinhala/SinhalaVowelLetterFixer.java
+++ src/main/java/corpus/sinhala/SinhalaVowelLetterFixer.java
@@ -1,6 +1,5 @@
 package corpus.sinhala;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- src/main/java/corpus/sinhala/Util.java
+++ src/main/java/corpus/sinhala/Util.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package solrcontroller;
+package corpus.sinhala;
 
 import java.io.BufferedReader;
 import java.io.File;
