cb0b0f74d1295b721409342c9078f1e3b0f18910
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-12-28 23:26:38 +0530

    project files added

---
+++ src/main/java/corpus/sinhala/App.java
@@ -0,0 +1,13 @@
+package corpus.sinhala.crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
