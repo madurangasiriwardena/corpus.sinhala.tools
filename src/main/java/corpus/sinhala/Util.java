de990660e74afe576a9afa2aadac9fbe09bd42c5
lasandun <lasadun@gmail.com>
2015-02-10 22:09:32 +0530

    Util.logger replaced by logger4j

--- src/main/java/corpus/sinhala/Util.java
+++ src/main/java/corpus/sinhala/Util.java
@@ -17,8 +17,7 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.text.Normalizer;
 import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.log4j.Logger;
 import java.util.regex.Pattern;
 
 /**
@@ -30,6 +29,7 @@ public class Util {
     String yansaya = "්" + "\u200d" + "ය"; // letter + yansaya + pillama
     String rakaraya = "්" + "\u200d" + "ර"; // letter + rakaraya + pillama
     String repaya = "ර" + "්" + "\u200d"; // repaya + letter
+    final static Logger logger = Logger.getLogger(Util.class);
     
     public static String refactorDirPath(String path) {
         if (path.charAt(path.length() - 1) != '/') {
@@ -87,11 +87,11 @@ public class Util {
                 content += line;
             }
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(ex);
         } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(ex);
         } catch (IOException ex) {
-            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(ex);
         }
         return content;
     }
@@ -107,7 +107,7 @@ public class Util {
             String s2 = new String(s1.replaceAll(regex, "").getBytes("ascii"), "ascii");
             return s2;
         } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(ex);
         }
         return null;
     }
